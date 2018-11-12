6b2ddc3e34cd38d0b0197eef8a409b693542f40e
localhost <localhost@qq.com>
2018-11-12 17:47:45 +0800

    commit neessy for tp5.local

---
+++ application/index/controller/Dynamic.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Neessy
+ * Date: 2018/11/9
+ * Time: 16:31
+ */
+
+namespace app\index\controller;
+
+
+use think\Controller;
+
+class Dynamic extends Controller
+{
+    /**
+     * @return view/dynamic/blog.html
+     * Desc:  博客列表页面
+     */
+    public function blog(){
+        $this->assign('title','公司博客');
+        return $this->fetch();
+    }
+
+    /**
+     * @return view/dynamic/portfolio.html
+     * Desc:  投资证券作品集，合作商户
+     */
+    public function portfolio(){
+        $this->assign('title','合作商户');
+        return $this->fetch();
+    }
+
+    /**
+     * @return view/dynamic/icons.html
+     * Desc:  网络图标大全页
+     */
+    public function icons(){
+        $this->assign('title','图标列表');
+        return $this->fetch();
+    }
+
+    /**
+     * @return view/dynamic/single.html
+     * Desc:  博客详情页
+     */
+    public function single(){
+        $this->assign('title','博客详情');
+        return $this->fetch();
+    }
+
+    /**
+     * @return view/dynamic/typo.html
+     * Desc:  打印，排版页面
+     */
+    public function typo(){
+        $this->assign('title','页面排版');
+        return $this->fetch();
+    }
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,54 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    /**
+     * @return view/index/index.html
+     * Desc:   首页
+     */
+    public function index()
+    {
+        $this->assign('title','首页');
+       return $this->fetch();
+    }
+
+    /**
+     * @return view/index/about.html
+     * Desc:  关于我们
+     */
+    public function about(){
+        $this->assign('title','关于我们');
+        return $this->fetch();
+    }
+
+    /**
+     * @return view/index/contact.html
+     * Desc:  联系我们
+     */
+    public function contact(){
+        $this->assign('title','联系我们');
+        return $this->fetch();
+    }
+
+    /**
+     * @return view/index/service.html
+     * Desc:  公司服务
+     */
+    public function services(){
+        $this->assign('title','公司服务');
+        return $this->fetch();
+    }
+
+    /**
+     * @param string $name
+     * @return hello name
+     * Desc:  ThinkPHP测试用方法
+     */
+    public function hello($name = 'ThinkPHP5')
+    {
+        return 'hello,' . $name;
+    }
+}
